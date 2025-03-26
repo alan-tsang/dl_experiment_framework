e8fe90c2c5037c8be9b5a77ecceb75e5fd6a231a
alan <leibzc@163.com>
2025-03-27 01:13:37 +0800

    add(bio util): generate mol img

--- zero2hero/common/__init__.py
+++ zero2hero/common/__init__.py
@@ -1,5 +1,6 @@
 from .registry import registry
 from .logger import Logger
-from .util import now, Namespace, set_seed, set_proxy
+from .util import now, Namespace, set_seed, set_proxy, better_dict_4_print
 from .io import load, dump, get_file_size, makedirs, cleanup_dir
 from .dl_util import get_gpu_usage, get_model_params_num, freeze_network, mean_pooling
+from .bio_util import *
