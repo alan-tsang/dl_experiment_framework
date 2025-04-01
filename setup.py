f47a574be5c78332fe8101d78a2b2966586a54ad
alan <leibzc@163.com>
2025-04-01 19:47:54 +0800

    emm

--- setup.py
+++ setup.py
@@ -10,20 +10,20 @@ setup(
     author_email='leibzc@163.com',
     description='personal deep learning experiment framework',
     install_requires=[
-        "torch>=2.1.2",
-        "wandb>=0.19.1",
-        "datasets>=3.3.2",
-        "omegaconf>2.3.0",
-        "nvidia_ml_py>=12.570.86",
-        "scikit_learn>=1.6.1"
-        "pynvml>=12.0.0",
-        "numpy>=1.21.1",
-        "rich>=13.9.4",
-        "fonttools>=4.55.6"
+        "torch",
+        "wandb",
+        "datasets",
+        "omegaconf",
+        "nvidia_ml_py",
+        "scikit_learn",
+        "pynvml",
+        "numpy",
+        "rich",
+        "fonttools"
     ],
     extras_require = {
-        "deep_speed": ["deepspeed>=0.16.2"],
-        "torch_geometric": ["torch_geometric>=2.0.1"],
-        "torch_scatter": ["torch_scatter>=2.1.2"],
+        "deep_speed": ["deepspeed"],
+        "torch_geometric": ["torch_geometric"],
+        "torch_scatter": ["torch_scatter"],
     },
 )
