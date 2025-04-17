21d29b372eb0d76826e6bb7565edb3684b7cdf99
alan <leibzc@163.com>
2025-04-17 12:54:36 +0800

    example update synchronously

--- example/use_transformer_to_copy_str.sh
+++ example/use_transformer_to_copy_str.sh
@@ -1,5 +1,7 @@
-torchrun --nproc_per_node 1 --master_port 12345\
-    ./use_transformer_to_copy_str.py\
-    your_want_add_arg1='{key: [1, 2, {subkey: "value"}]}'\
-    your_want_add_arg2=value2\
-    your_want_add_arg3=value3\
+torchrun \
+--nproc_per_node 1 \
+--master_port 12345 \
+./use_transformer_to_copy_str.py \
+your_want_add_arg1='{key: [1, 2, {subkey: "value"}]}' \
+your_want_add_arg2=value2 \
+your_want_add_arg3=value3
